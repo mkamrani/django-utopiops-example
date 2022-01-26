e380581b2efb7e3ebb054bba8211a1635a78850e
Mohsen Kamrani <mohsen.kamrani@austcommunitymedia.com.au>
2022-01-26 21:38:10 +1100

    Set the ALLOWED_HOSTS

--- django_utopiops_example/settings.py
+++ django_utopiops_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-32mjivb_yrjzm+(-d45)1qtx+egi9+*fa63yebxr@$pymyvq+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
